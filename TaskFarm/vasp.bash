852ed8b02f2e0064921ea90df8d75f9be23fdc69
Keith Butler <k.t.butler@bath.ac.uk>
2016-04-18 09:58:42 +0100

    Annotated the files

--- TaskFarm/vasp.bash
+++ TaskFarm/vasp.bash
@@ -1,13 +1,16 @@
+# The variables are red in from the command in farm.bash.
 export NPROC2=$1
 export dir=$2
 echo $dir
 cd $dir
 echo `pwd`
+# This tests if the VASP calculation has previously completed in this directory.
 finished_calc=`grep 'free energy' OUTCAR | awk '{print $2}'`
 if [ "$finished_calc" == 'energy' ]
 then
     echo "Calculation done already!"
 else
+# This is just the way that this particular script runs the job, change as you please.
     cp ../INCAR ../POTCAR ../KPOINTS .
     cp *POSCAR-* POSCAR
     aprun -n $NPROC2 -N 24 vasp_std > vasp.out
